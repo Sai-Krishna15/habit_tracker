7ac63ca92ed3bd91dde67537b4009145bde00c16
Sai-Krishna15 <saikrishna.siginamsetty@gohyper.space>
2025-06-04 13:06:05 +0530

    Quick Fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || habits_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
